4cd564ebc07c877ad95e42873dc1dd7c8ec464a3
jiayu wu <>
2018-02-22 00:17:50 -0500

    week6changes

---
+++ js/form-handler.js
@@ -0,0 +1,8 @@
+function capture() {
+    event. preventDefault()
+    console. log ("form submitted")
+    
+    var theEmail = document.custInfo.email.value
+    console.log ("thank you for signing up as" + theEmail)
+    alert ("Thank you for signing up as" + theEmail)
+}
