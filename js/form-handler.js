da8b1bbb52239d8c12c8eb5aeb096c6b3d19b84a
jiayu wu <>
2018-03-13 23:39:22 -0400

    redid

--- js/form-handler.js
+++ js/form-handler.js
@@ -1,12 +1,51 @@
-function addtocart() {
-    document.getElementById("added").innerHTML = "1 item added to cart";
-}
 
-function removefromcart() {
-    document.getElementById("remove").innerHTML = "1 item removed from cart";
-}
+var shoppingCart = []
 
 
-function viewCart() {
-    document.getElementById("cart").innerHTML = "1 item in your cart now";
+var totalPrice = 0
+
+
+function add (itemFullName,price) {
+  event.preventDefault()
+  
+  
+  var i = shoppingCart.indexOf(itemFullName)
+  
+  
+  if(i == -1) {
+    shoppingCart.push(itemFullName)
+    totalPrice = totalPrice + price
+    } 
+  
+  else {
+    shoppingCart.splice(i,1)
+   
+    }
+  
+  
+  shoppingCart = shoppingCart.sort()
+  
+  
+  if(totalPrice < 0 && totalPrice > -0.1) {
+    totalPrice = 0
+  }
+  
+  
+  console.log(shoppingCart.length + " items added to cart, your payment is going to be $" + totalPrice.toFixed(2) )
+  
+ 
+  if(shoppingCart.length != 0) {
+    document.querySelector("#cartcount").innerHTML = ("  (" + shoppingCart.length + ")")
+  }
+  
+  else {
+    document.querySelector("#cartcount").innerHTML = ""
+  }
+
 }
+
+
+function cartNow() {
+  console.log("In your cart: " + shoppingCart.join(", "))
+}
+
